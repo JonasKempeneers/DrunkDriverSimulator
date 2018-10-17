f9f815375a94cfe49addab3f62c7913ef866d759
JonathanLauwers <Lauwersjon@gmail.com>
2018-10-17 11:32:16 +0200

    Movement of car

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 10f;
+
+
+    private void Update()
+    {
+        transform.Translate(0f, 0f, moveSpeed * 20 *Input.GetAxis("Vertical")*Time.deltaTime);
+        transform.Rotate( 0f, rotateSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f);
+    }
+}
