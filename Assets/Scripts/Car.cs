1d2202d5cf7142a14f2a8d19aaba24b0557464e3
JonathanLauwers <Lauwersjon@gmail.com>
2018-10-17 15:10:41 +0200

    Implementing of moving enemies

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class Car : MonoBehaviour
 {
@@ -11,28 +13,41 @@ public class Car : MonoBehaviour
 
     public float rotateSpeed = 5f;
 
+    public GameObject canvas;
+    private bool gameOver;
 
     private void Update()
     {
-        Debug.Log(speed);
+
         if ((Input.GetKey("down")) && (speed < maxSpeed))
-            speed = speed - acceleration * Time.deltaTime;
-        else if ((Input.GetKey("up")) && (speed < maxSpeed))
-            speed = speed + acceleration * Time.deltaTime;
-        else
-        {
-            if (speed > deceleration * Time.deltaTime)
-            speed = speed - deceleration * Time.deltaTime;
-            else if (speed < -deceleration * Time.deltaTime)
-            speed = speed + deceleration * Time.deltaTime;
+                speed = speed - acceleration * Time.deltaTime;
+            else if ((Input.GetKey("up")) && (speed < maxSpeed))
+                speed = speed + acceleration * Time.deltaTime;
             else
-            speed = 0;
+            {
+                if (speed > deceleration * Time.deltaTime)
+                    speed = speed - deceleration * Time.deltaTime;
+                else if (speed < -deceleration * Time.deltaTime)
+                    speed = speed + deceleration * Time.deltaTime;
+                else
+                    speed = 0;
+            }
+            transform.Translate(0f, 0f, speed);
+            transform.Rotate(0f, rotateSpeed * 2 * Input.GetAxis("Horizontal") * Time.deltaTime, 0f);
+            
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enemy" || gameObject.transform.rotation.z > 90f)
+        {
+            GameOver();
         }
+    }
 
-
-
-
-        transform.Translate(0f, 0f, speed);
-        transform.Rotate( 0f, rotateSpeed * 2* Input.GetAxis("Horizontal") * Time.deltaTime, 0f);
+    private void GameOver()
+    {
+        canvas.SetActive(true);
     }
 }
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    private void Update()
+    {
+
+       
+        transform.Translate(0f, 0f, 0.1f);
+
+        
+    }
+}
