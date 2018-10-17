2b2f8bd4cbf1569669fee8e69704666c6f0d61c5
JonathanLauwers <Lauwersjon@gmail.com>
2018-10-17 11:50:18 +0200

    Accelerating/decelerating

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
+    private float speed = 0f;
+    private float maxSpeed = 3f;
+    private float acceleration = 1f;
+    private float deceleration = 1f;
+
     public float moveSpeed = 5f;
-    public float rotateSpeed = 10f;
+    public float rotateSpeed = 5f;
 
 
     private void Update()
     {
-        transform.Translate(0f, 0f, moveSpeed * 20 *Input.GetAxis("Vertical")*Time.deltaTime);
-        transform.Rotate( 0f, rotateSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f);
+        if ((Input.GetKey("down")) && (speed < maxSpeed))
+            speed = speed - acceleration * Time.deltaTime;
+        else if ((Input.GetKey("up")) && (speed > -maxSpeed))
+            speed = speed + acceleration * Time.deltaTime;
+        else
+        {
+            if (speed > deceleration * Time.deltaTime)
+            speed = speed - deceleration * Time.deltaTime;
+            else if (speed < -deceleration * Time.deltaTime)
+            speed = speed + deceleration * Time.deltaTime;
+            else
+            speed = 0;
+        }
+
+
+
+
+        transform.Translate(0f, 0f, speed);
+        transform.Rotate( 0f, rotateSpeed * 2* Input.GetAxis("Horizontal") * Time.deltaTime, 0f);
     }
 }
